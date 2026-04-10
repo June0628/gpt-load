50bbdd9f87ad389c3c3220abd484d2fe0bfd9ee2
Jun <l1696221535@gmail.com>
2026-04-10 10:05:51 +0800

    fix: 移除请求体日志的65000字符限制

--- internal/proxy/server.go
+++ internal/proxy/server.go
@@ -305,7 +305,7 @@ func (ps *ProxyServer) logRequest(
 	var requestBodyToLog, userAgent string
 
 	if group.EffectiveConfig.EnableRequestBodyLogging {
-		requestBodyToLog = utils.TruncateString(string(bodyBytes), 65000)
+		requestBodyToLog = string(bodyBytes)
 		userAgent = c.Request.UserAgent()
 	}
 
